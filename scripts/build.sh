b1703328597a99303c8ffe4cbb9c377d23f4a31a
Matthew Downs <matthew@neer.ai>
2022-03-09 16:02:58 -0600

    Fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,12 +10,12 @@ fi
 # Determine the git repo and tag to use based on the VERSION variable
 if [[ ${VERSION} < 5.2 ]]
 then
-    # Specified EPASWMM version is less than 5.2.0, use USEPA repository
+    echo "Specified EPASWMM version is less than 5.2.0, using USEPA repository"
     GIT_REPO_URL="https://github.com/USEPA/Stormwater-Management-Model.git"
     GIT_REPO_TAG="v${VERSION}"
 elif [[ $VERSION == 5.2.0 ]]
 then
-    # Specified EPASWMM version is 5.2.0, use OpenWaterAnalytics repository
+    echo "Specified EPASWMM version is 5.2.0, using OpenWaterAnalytics repository"
     GIT_REPO_URL="https://github.com/OpenWaterAnalytics/Stormwater-Management-Model.git"
     GIT_REPO_TAG="v${VERSION}.dev7"
 else
@@ -24,21 +24,26 @@ else
 fi
 
 # Add a tag suffix based on os/arch
-# Apple M1 chip allows Rosetta, which is a disguised amd64 shell, however, even
-# when building the image under Rosetta, it will always be an arm64 image
-# due to CPU architecture restrictions
-if [[ $(uname -m) == "x86_64" ]]; then
-    if /usr/bin/pgrep oahd >/dev/null 2>&1; then
-        TAG_SUFFIX="-arm64-linux"
+if /usr/bin/pgrep oahd >/dev/null 2>&1; then
+    # The Apple M1 chip allows Rosetta, which is a disguised amd64 shell, however,
+    # even when building the image under Rosetta, it will always be an arm64 image
+    # due to CPU architecture restrictions
+    ARCHITECTURE="arm64"
+elif [[ $(arch) == "i386" ]]; then
+    # macOS can still report `arch` as "i386", even on 64-bit machines
+    # so we need to check the `uname -m` command if it is a 64-bit machine
+    # if it is, then manually set it as "amd64"
+    if [[ $(uname -m) == "x86_64" ]]; then
+        ARCHITECTURE="amd64"
     else
-        TAG_SUFFIX="-amd64-linux"
+        ARCHITECTURE="i386"
     fi
 else
-    TAG_SUFFIX="-$(arch)-linux"
+    ARCHITECTURE=$(arch)
 fi
 
 # Build the image with arguments and tag it
 docker build \
     --build-arg GIT_REPO_URL=${GIT_REPO_URL} \
     --build-arg GIT_REPO_TAG=${GIT_REPO_TAG} \
-    --tag neerteam/epaswmm:${VERSION}${TAG_SUFFIX} .
+    --tag neerteam/epaswmm:${VERSION}-linux-${ARCHITECTURE} .
