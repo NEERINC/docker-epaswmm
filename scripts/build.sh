bd72d8c597468f39ee67db40e806d1d881d2a09b
Matthew Downs <matthew@neer.ai>
2022-03-09 14:50:50 -0600

    Fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,7 +27,8 @@ fi
 # Apple M1 chip allows Rosetta, which is a disguised amd64 shell, however, even
 # when building the image under Rosetta, it will always be an arm64 image
 # due to CPU architecture restrictions
-if [[ "$(sysctl -n sysctl.proc_translated)" == "1" ]]
+PROC_TRANSLATED=$(sysctl -n sysctl.proc_translated) || 0
+if [[ $PROC_TRANSLATED == 1 ]]
 then
     TAG_SUFFIX="-arm64-linux"
 else
