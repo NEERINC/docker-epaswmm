adabfb4d295c5644e5c82d43b4d6db07dba93d0a
Matthew Downs <matthew@neer.ai>
2022-03-09 15:06:17 -0600

    Add echos

--- scripts/build-and-push-all.sh
+++ scripts/build-and-push-all.sh
@@ -2,12 +2,15 @@
 
 # 5.1.14
 IMAGE=$(VERSION=5.1.14 $(dirname "$0")/build.sh)
+echo "Pushing ${IMAGE}"
 docker push $IMAGE
 
 # 5.1.15
 IMAGE=$(VERSION=5.1.15 $(dirname "$0")/build.sh)
+echo "Pushing ${IMAGE}"
 docker push $IMAGE
 
 # 5.2.0
 IMAGE=$(VERSION=5.2.0 $(dirname "$0")/build.sh)
+echo "Pushing ${IMAGE}"
 docker push $IMAGE
