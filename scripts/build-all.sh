a09c825be75eef20d2bb48380e6ddc5c3fa8e34e
Matthew Downs <matthew@neer.ai>
2022-03-09 13:56:30 -0600

    Update scripts

---
+++ scripts/build-all.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# 5.1.14
+IMAGE=$(VERSION=5.1.14 $(dirname "$0")/build.sh)
+docker push $IMAGE
+
+# 5.1.15
+IMAGE=$(VERSION=5.1.15 $(dirname "$0")/build.sh)
+docker push $IMAGE
+
+# 5.2.0
+IMAGE=$(VERSION=5.2.0 $(dirname "$0")/build.sh)
+docker push $IMAGE
--- scripts/build.sh
+++ scripts/build.sh
@@ -10,12 +10,12 @@ fi
 # Determine the git repo and tag to use based on the VERSION variable
 if [[ ${VERSION} < 5.2 ]]
 then
-    echo "Specified EPASWMM version is less than 5.2.0, using USEPA repository"
+    # Specified EPASWMM version is less than 5.2.0, use USEPA repository
     GIT_REPO_URL="https://github.com/USEPA/Stormwater-Management-Model.git"
     GIT_REPO_TAG="v${VERSION}"
 elif [[ $VERSION == 5.2.0 ]]
 then
-    echo "Specified EPASWMM version is 5.2.0, using OpenWaterAnalytics repository"
+    # Specified EPASWMM version is 5.2.0, use OpenWaterAnalytics repository
     GIT_REPO_URL="https://github.com/OpenWaterAnalytics/Stormwater-Management-Model.git"
     GIT_REPO_TAG="v${VERSION}.dev7"
 else
@@ -23,19 +23,15 @@ else
     exit 1
 fi
 
-# Use an additional tag suffix if the OS architecture is not
-MACHINE_HARDWARE_NAME=$(uname -m)
+# Add a tag suffix based on os/arch
+# Apple M1 chip allows Rosetta, which is a disguised amd64 shell, however, even
+# when building the image under Rosetta, it will always be an arm64 image
+# due to CPU architecture restrictions
 if [[ $(sysctl -n machdep.cpu.brand_string) =~ "Apple M1" ]]
 then
-    TAG_SUFFIX="-arm64"
-    echo "Detected Apple M1 chip, using tag suffix \"${TAG_SUFFIX}\""
-elif [ "${MACHINE_HARDWARE_NAME}" != "x86_64" ]
-then
-    TAG_SUFFIX="-${MACHINE_HARDWARE_NAME}"
-    echo "Detected a non x86_64 architecture, using tag suffix \"${TAG_SUFFIX}\""
+    TAG_SUFFIX="-arm64-linux"
 else
-    echo "Detected x86_64 architecture, no tag suffix required"
-    TAG_SUFFIX=""
+    TAG_SUFFIX="-$(arch)-linux"
 fi
 
 # Build the image with arguments and tag it
@@ -43,5 +39,6 @@ docker build \
     --no-cache \
     --build-arg GIT_REPO_URL=${GIT_REPO_URL} \
     --build-arg GIT_REPO_TAG=${GIT_REPO_TAG} \
-    --progress plain \
-    -t neerteam/epaswmm:${VERSION}${TAG_SUFFIX} .
+    --tag neerteam/epaswmm:${VERSION}${TAG_SUFFIX} .
+
+echo "neerteam/epaswmm:${VERSION}${TAG_SUFFIX}"
