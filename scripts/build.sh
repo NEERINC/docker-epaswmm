5b9adbafae1c44b4da2de056583547a78ad1f80f
Matthew Downs <matthew@neer.ai>
2022-03-09 12:27:27 -0600

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,17 +6,23 @@ if [[ -z "${DOCKER_EPASWMM_VERSION}" ]]; then
     exit 1
 fi
 
+# Use an additional tag suffix if the OS architecture is not
 MACHINE_HARDWARE_NAME=$(uname -m)
-if [[ $(sysctl -n machdep.cpu.brand_string) =~ "Apple M1" ]]; then
-    OS_ARCHITECTURE_SUFFIX="-arm64"
-elif [[ "${MACHINE_HARDWARE_NAME}" != "amd64" ]]; then
-    OS_ARCHITECTURE_SUFFIX="-${MACHINE_HARDWARE_NAME}"
+if [[ $(sysctl -n machdep.cpu.brand_string) =~ "Apple M1" ]]
+then
+    OS_ARCHITECTURE_TAG_SUFFIX="-arm64"
+    echo "Detected Apple M1 chip, using tag suffix \"${OS_ARCHITECTURE_TAG_SUFFIX}\""
+elif [ "${MACHINE_HARDWARE_NAME}" != "x86_64" ]
+then
+    OS_ARCHITECTURE_TAG_SUFFIX="-${MACHINE_HARDWARE_NAME}"
+    echo "Detected a non x86_64 architecture, using tag suffix \"${OS_ARCHITECTURE_TAG_SUFFIX}\""
 else
-    OS_ARCHITECTURE_SUFFIX=""
+    echo "Detected x86_64 architecture, no tag suffix required"
+    OS_ARCHITECTURE_TAG_SUFFIX=""
 fi
 
 # Build the image with the tag set to the DOCKER_EPASWMM_VERSION varaible
 docker build . \
     --no-cache \
     --build-arg VERSION=${DOCKER_EPASWMM_VERSION} \
-    -t neerteam/epaswmm:${DOCKER_EPASWMM_VERSION}${OS_ARCHITECTURE_SUFFIX}
+    -t neerteam/epaswmm:${DOCKER_EPASWMM_VERSION}${OS_ARCHITECTURE_TAG_SUFFIX}
