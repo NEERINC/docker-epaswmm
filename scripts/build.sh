8adf201c8e9f68093cb4226a0f306131c4fde93d
Matthew Downs <matthew@neer.ai>
2022-03-09 12:10:11 -0600

    Add check in build script to detect architecture

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,17 @@ if [[ -z "${DOCKER_EPASWMM_VERSION}" ]]; then
     exit 1
 fi
 
+MACHINE_HARDWARE_NAME=$(uname -m)
+if [[ $(sysctl -n machdep.cpu.brand_string) =~ "Apple M1" ]]; then
+    OS_ARCHITECTURE_SUFFIX="-arm64"
+elif [[ "${MACHINE_HARDWARE_NAME}" != "amd64" ]]; then
+    OS_ARCHITECTURE_SUFFIX="-${MACHINE_HARDWARE_NAME}"
+else
+    OS_ARCHITECTURE_SUFFIX=""
+fi
+
 # Build the image with the tag set to the DOCKER_EPASWMM_VERSION varaible
-docker build --no-cache --build-arg VERSION=$DOCKER_EPASWMM_VERSION -t neerteam/epaswmm:$DOCKER_EPASWMM_VERSION .
+docker build . \
+    --no-cache \
+    --build-arg VERSION=${DOCKER_EPASWMM_VERSION} \
+    -t neerteam/epaswmm:${DOCKER_EPASWMM_VERSION}${OS_ARCHITECTURE_SUFFIX}
